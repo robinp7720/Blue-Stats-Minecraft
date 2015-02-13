d1c488d3407c3426cff7632bd1f5b9d64034aec1
Robin Decker <robin@decker.cx>
2015-02-13 19:49:18 +0000

    Fix player page colorinng

--- parts/footer.php
+++ parts/footer.php
@@ -1,3 +1,5 @@
-<?php echo '<div class="center"><b>Total Execution Time:</b> '.$execution_time.' Secs</div>'; ?>
+<div class="container">
+	<?php echo '<div class="center"><b>Total Execution Time:</b> '.$execution_time.' Secs</div>'; ?>
+</div>
 </body>
 </html>
