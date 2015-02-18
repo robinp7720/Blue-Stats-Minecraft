1b9550935866ce0380ddec26ebdfe5a1e84cf772
robinp7720 <robin@decker.cx>
2015-02-18 11:21:05 +0000

    Added footer and other ui changes

--- parts/footer.php
+++ parts/footer.php
@@ -1,3 +1,12 @@
 
-</body>
+		<footer class="footer">
+	    	<div class="container">
+	        	<p class="text-muted">Made with love by &nbsp;<img class="player-head-player_page" src="http://cravatar.eu/avatar/_OvErLoRd_/16" alt=""> &nbsp; <b>_OvErLoRd_</b><br>
+	      			<br>
+	      			<b>Github: </b><a href="https://github.com/robinp7720/Blue-Stats-Minecraft"> https://github.com/robinp7720/Blue-Stats-Minecraft</a><br>
+	      			<b>Bukkit forums: </b><a href="http://bukkit.org/threads/blue-stats.334006/"> http://bukkit.org/threads/blue-stats.334006/</a>
+	      		</p>
+	      	</div>
+	    </footer>
+	</body>
 </html>
--- parts/head.php
+++ parts/head.php
@@ -10,6 +10,8 @@
 	<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.2/flatly/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="//cdn.datatables.net/plug-ins/f2c75b7247b/integration/bootstrap/3/dataTables.bootstrap.css">
 
+	<link href='css/custom.css' rel='stylesheet' type='text/css'>
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 	<script src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
