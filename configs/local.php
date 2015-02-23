f5bec90176ddc8d03daed91029e56fba526f8d78
robinp7720 <robin@decker.cx>
2015-02-23 00:10:31 +0000

    Added missing english locals

--- configs/local.php
+++ configs/local.php
@@ -5,3 +5,8 @@ $localization["nav"]["highscores"]="High Scores";
 $localization["nav"]["allplayers"]="All Players";
 $localization["nav"]["pvp"]="PvP Stats";
 $localization["nav"]["blocks"]="Block Stats";
+
+/* High Scores */
+$localization["highscores"]["place"]="Place";
+$localization["highscores"]["player"]="Player";
+$localization["highscores"]["status"]="Status";
