a08402c318822c1e16bf8f6a53390e8c355e380c
robinp7720 <robin@decker.cx>
2015-03-09 23:21:32 +0000

    more changes

---
+++ modules/highscores/highscores.php
@@ -0,0 +1,59 @@
+<div class="row">
+<?php
+foreach ($BlueStats->config["highscores"]["highscores"] as $highscores_index => $highscores_item) :
+	$highscore = get_highscore($mysqli,$BlueStats->config["mysql"]["stats"]["table_prefix"],$highscores_item["stat"],$highscores_item["amount"]);
+	$title = str_replace ('{AMOUNT}',$highscores_item["amount"],$BlueStats->config["highscores"]["title"]);
+	$title = str_replace ('{STAT}',$BlueStats->config["stats"]["names"][$highscores_item["stat"]],$title);
+	?>
+
+
+	<section class="col-md-6">
+		<h2><?=$title; ?></h2>
+
+		<table class="table table-striped table-bordered">
+			<thead>
+				<tr>
+					<th><?=$localization["highscores"]["place"]?></th>
+					<th><?=$localization["highscores"]["player"]?></th>
+					<?php if ($config[$serverId]["server"]["query_enabled"]):?><th><?=$localization["highscores"]["status"]?></th><?php endif; ?>
+					<th><?=$config[$serverId]["stats"]["names"][$highscores_item["stat"]]; ?></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($highscore as $item => $player) :?>
+				<?php 
+				if ($highscores_item["stat"]=="playtime"){
+					$stat = secondsToTime($player[$highscores_item["stat"]]);
+				}else{
+					$stat= $player[$highscores_item["stat"]];
+				}
+
+				$image_url = player_face($player["name"],$config[$serverId]["faces"]["highscores"]["size"],$config[$serverId]["faces"]["highscores"]["url"]);
+				?>
+				<tr>
+					<td class="highscore-place"><?=$item+1; ?></td>
+					<td>
+						<a href="<?= $BlueStats->makePlayerUrl($player["player_id"])?>">
+							<img src="<?=$image_url?>" alt="<?=$player["name"]?>"/>
+							 <?=$player["name"]?>
+						</a>
+					</td>
+					<?php if (isset($Online_Players)): ?>
+					<td>
+						<?php if (playerOnline($player["name"], $Online_Players)): ?>
+						<span class="label label-success">Online</span>
+						<?php else: ?>
+						<span class="label label-danger">Offline</span>
+						<?php endif; ?>
+					</td>
+					<?php endif; ?>
+					<td><?=$stat; ?></td>
+				</tr>
+				<?php endforeach ?>
+			</tbody>
+		</table>
+	</section>
+
+
+<?php endforeach ?>
+</div>
--- parts/footer.php
+++ parts/footer.php
@@ -1,11 +1,22 @@
 
 		<footer class="footer">
 	    	<div class="container">
-	        	<p class="text-muted">Made with love by &nbsp;<img class="player-head-player_page" src="http://cravatar.eu/avatar/_OvErLoRd_/16" alt=""> &nbsp; <b>_OvErLoRd_</b><br>
-	      			<br>
-	      			<b>Github: </b><a href="https://github.com/robinp7720/Blue-Stats-Minecraft"> https://github.com/robinp7720/Blue-Stats-Minecraft</a><br>
-	      			<b>Bukkit forums: </b><a href="http://bukkit.org/threads/blue-stats.334006/"> http://bukkit.org/threads/blue-stats.334006/</a>
-	      		</p>
+	    		<div class="row">
+	    			<div class="col-sm-6">
+			        	<p class="text-muted">Made with love by &nbsp;<img class="player-head-player_page" src="http://cravatar.eu/avatar/_OvErLoRd_/16" alt=""> &nbsp; <b>_OvErLoRd_</b><br>
+			      			<br>
+			      			<b>Github: </b><a href="https://github.com/robinp7720/Blue-Stats-Minecraft"> https://github.com/robinp7720/Blue-Stats-Minecraft</a><br>
+			      			<b>Bukkit forums: </b><a href="http://bukkit.org/threads/blue-stats.334006/"> http://bukkit.org/threads/blue-stats.334006/</a>
+			      		</p>
+			      	</div>
+			      	<div class="col-sm-6">
+			        	<p class="text-muted">
+			        		Made possible by the all amazing &nbsp;<img src="http://dev.bukkit.org/thumbman/avatars/9/401/32x32/LifeWave.jpg.-m0.png" alt="Avatar of Lolmewn"> &nbsp; <b>lolmewn</b>!</br>
+			        		<br>
+			      			<b>lolmewnstats: </b><a href="https://github.com/robinp7720/Blue-Stats-Minecraft"> http://dev.bukkit.org/bukkit-plugins/lolmewnstats/</a><br>
+			      		</p>
+			      	</div>
+		      	</div>
 	      	</div>
 	    </footer>
 	</body>
