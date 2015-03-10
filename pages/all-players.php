ed1d8e712b34caa6477efa8a56d7771ce0095e18
robinp7720 <robin@decker.cx>
2015-03-10 17:29:45 +0000

    Upgrade allplayers page to the new modular system

--- pages/all-players.php
+++ pages/all-players.php
@@ -1,42 +1,47 @@
+<?php
+$page = "allplayers";
+$strRepl = array(
+	"serverName" => $BlueStats->config["server"]["server_name"],
+);
 
-<table class="table table-striped table-bordered" id="all-players">
-	<thead>
-		<tr>
-			<th>Player</th>
-			<?php if($config[$serverId]["server"]["query_enabled"]){echo"<th>Status</th>";}?>
-			<th><?=$config[$serverId]["stats"]["names"][$config[$serverId]["allPlayers"]["defaultStat"]]; ?></th>
-			<th>Value</th>
-		</tr>
-	</thead>
-	<tfoot>
-		<tr>
-			<th>Player</th>
-			<?php if($config[$serverId]["server"]["query_enabled"]){echo"<th>Status</th>";}?>
-			<th><?=$config[$serverId]["stats"]["names"][$config[$serverId]["allPlayers"]["defaultStat"]]; ?></th>
-			<th>Value</th>
-		</tr>
-	</tfoot>
-</table>
+$string = file_get_contents($BlueStats->appPath."/page-templates/$page.html");
 
-<script type="text/javascript">
-	$(document).ready(function() {
-	    $('#all-players').dataTable( {
-	    	<?php /* If url rewrites have been disabled */ if ($config[$serverId]["url"]["rewrite"]==false) :?>
-	        "ajax": './ajax/call.php?func=allplayers',
-	        <?php else: ?>
-	        "ajax": '<?=$config[$serverId]["url"]["base"]?>/ajax/?func=allplayers',
-	        <?php endif; ?>
-	        <?php if ($config[$serverId]["server"]["query_enabled"]):?>
-	        "aoColumnDefs": [
-		      { "iDataSort": 3, "aTargets": [ 2 ] },
-		      { "bVisible": false, "aTargets": [ 3 ] }
-		    ]
-		    <?php else: ?>
-		    "aoColumnDefs": [
-		      { "iDataSort": 2, "aTargets": [ 1 ] },
-		      { "bVisible": false, "aTargets": [ 2 ] }
-		    ]
-		    <?php endif; ?>
-	    } );
-	} );	
-</script>
+foreach ($strRepl as $repl => $new){
+	$string = str_replace("{{ text:".$repl." }}", $new, $string);
+}
+
+/* Modules */
+preg_match_all('/{{ module:([^ ]+) }}/', $string, $matches);
+foreach ($matches[1] as $key => $filename) {
+    //replace content:
+    ob_start();
+    include($BlueStats->appPath."/modules/$page/$filename.php");
+    $contents = ob_get_contents();
+    ob_end_clean();
+    $string = str_replace($matches[0][$key], $contents, $string);
+}
+
+/* Global Modules */
+preg_match_all('/{{ Gmodule:([^ ]+) }}/', $string, $matches);
+foreach ($matches[1] as $key => $filename) {
+    //replace content:
+    ob_start();
+    include($BlueStats->appPath."/modules/global/$filename.php");
+    $contents = ob_get_contents();
+    ob_end_clean();
+    $string = str_replace($matches[0][$key], $contents, $string);
+}
+
+/* Urls */
+preg_match_all('/{{ url:([^ ]+) }}/', $string, $matches);
+foreach ($matches[1] as $key => $site) {
+	if ($config[$serverId]["url"]["rewrite"]==false){
+		$url = "?page=allplayers";
+	}else{
+		$url = $BlueStats->config["url"]["base"]."/$site/";
+	}
+
+    $string = str_replace($matches[0][$key], $url, $string);
+}
+
+echo $string;
