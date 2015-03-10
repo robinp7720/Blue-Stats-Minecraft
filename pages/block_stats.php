d4bc33c38ffddeff92e6f296010902f3e4b64315
robinp7720 <robin@decker.cx>
2015-03-10 18:30:49 +0000

    Upgaded block stats page to new modular system

--- pages/block_stats.php
+++ pages/block_stats.php
@@ -1,72 +1,47 @@
+<?php
+$page = "blocks";
+$strRepl = array(
+	"serverName" => $BlueStats->config["server"]["server_name"],
+);
 
-<h2>Block Stats</h2>
-<table class="table table-striped table-bordered" id="blockstats">
-	<thead>
-		<tr>
-			<th>Block</th>
-			<th>Amount of times placed</th>
-			<th>Amount of times broken</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-			/* Get all block stats from player */
-			$blocks_raw = globalBlockStats($mysqli,$config[$serverId]["mysql"]["stats"]["table_prefix"]);
-			$blocks= array();
+$string = file_get_contents($BlueStats->appPath."/page-templates/$page.html");
 
-			/* If no blocks have been placed, ignore this */
-			if (!empty($blocks_raw)){
-				/* Loop through all blocks to make it use-able. */
-				foreach ($blocks_raw as $item => $value){
-					if ($value["break"]==1){
-						/* Block Broken */
-						if (isset($blocks[$value["blockID"]]["broken"])){
-							$blocks[$value["blockID"]]["broken"]+=$value["amount"];
-						}else{
-							$blocks[$value["blockID"]]["broken"]=$value["amount"];
-						}
-						if (!isset($blocks[$value["blockID"]]["placed"])){
-							$blocks[$value["blockID"]]["placed"]=0;
-						}
-					}else{
-						/* block Placed */
-						if (isset($blocks[$value["blockID"]]["placed"])){
-							$blocks[$value["blockID"]]["placed"]+=$value["amount"];
-						}else{
-							$blocks[$value["blockID"]]["placed"]=$value["amount"];
-						}
-						if (!isset($blocks[$value["blockID"]]["broken"])){
-							$blocks[$value["blockID"]]["broken"]=0;
-						}
-					}
-				}
-			}
+foreach ($strRepl as $repl => $new){
+	$string = str_replace("{{ text:".$repl." }}", $new, $string);
+}
 
-			foreach ($blocks as $item => $value) :
-				$block_name = strtolower(getMaterialFromId($item,0,$blocks_names)); 
-			?>
-			<tr>
-			<td>
-			<?php 
-				/* If block icons are turned on add them to the html here */
-				if ($config[$serverId]["blocks"]["displayIcons"]==true){
-					echo '<img class="block-icon" src="'."images/blocks/".$item.'-0.png"/> ';
-				}
+/* Modules */
+preg_match_all('/{{ module:([^ ]+) }}/', $string, $matches);
+foreach ($matches[1] as $key => $filename) {
+    //replace content:
+    ob_start();
+    include($BlueStats->appPath."/modules/$page/$filename.php");
+    $contents = ob_get_contents();
+    ob_end_clean();
+    $string = str_replace($matches[0][$key], $contents, $string);
+}
 
-				echo $block_name;
-			?>
-			</td>
-			<td><?=$value["placed"] ?></td>
-			<td><?=$value["broken"] ?></td>
-		</tr>
-		<?php endforeach; ?>
-	</tbody>
-</table>
+/* Global Modules */
+preg_match_all('/{{ Gmodule:([^ ]+) }}/', $string, $matches);
+foreach ($matches[1] as $key => $filename) {
+    //replace content:
+    ob_start();
+    include($BlueStats->appPath."/modules/global/$filename.php");
+    $contents = ob_get_contents();
+    ob_end_clean();
+    $string = str_replace($matches[0][$key], $contents, $string);
+}
 
-<script>
-	$(document).ready(function() {
-		$('#blockstats').dataTable({
-			responsive: true
-		});
-	} );
-</script>
+/* Urls */
+preg_match_all('/{{ url:([^ ]+) }}/', $string, $matches);
+foreach ($matches[1] as $key => $site) {
+	if ($config[$serverId]["url"]["rewrite"]==false){
+		$url = "?page=allplayers";
+	}else{
+		$url = $BlueStats->config["url"]["base"]."/$site/";
+	}
+
+    $string = str_replace($matches[0][$key], $url, $string);
+}
+
+echo $string;
