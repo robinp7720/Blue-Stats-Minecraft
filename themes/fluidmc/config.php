d7315cc863792c23ddfef86ae13125b7922dac0d
robinp7720 <robin@decker.cx>
2015-03-10 22:35:54 +0000

    fluid mc stats theme

---
+++ themes/fluidmc/config.php
@@ -0,0 +1,30 @@
+<?php
+$theme["name"] = "Fluid MC stats";
+$theme["nav"]["color"]=array(
+	"red" => 34,
+	"green" => 34,
+	"blue" => 34
+);
+
+$theme["nav"]["youtube"]=false;
+
+$theme["headers"]["color"]=array(
+	"red" => 33,
+	"green" => 150,
+	"blue" => 243
+);
+$theme["pager"]["color"]=array(
+	"red" => 33,
+	"green" => 150,
+	"blue" => 243
+);
+$theme["background"]["color"]=array(
+	"red" => 50,
+	"green" => 16,
+	"blue" => 121
+);
+/* True = Full width False = not full width? */
+$theme["container"]["body"]["fluid"]=true;
+$theme["container"]["nav"]["fluid"]=true;
+
+$theme["creator"] = "robinp7720";
