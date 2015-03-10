ae228b8b2ef72502eb4b8cd8f629deb1b00a4743
robinp7720 <robin@decker.cx>
2015-03-10 17:29:45 +0000

    Upgrade allplayers page to the new modular system

---
+++ modules/allplayers/allplayers.php
@@ -0,0 +1,42 @@
+
+<table class="table table-striped table-bordered" id="all-players">
+	<thead>
+		<tr>
+			<th>Player</th>
+			<?php if($config[$serverId]["server"]["query_enabled"]){echo"<th>Status</th>";}?>
+			<th><?=$config[$serverId]["stats"]["names"][$config[$serverId]["allPlayers"]["defaultStat"]]; ?></th>
+			<th>Value</th>
+		</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<th>Player</th>
+			<?php if($config[$serverId]["server"]["query_enabled"]){echo"<th>Status</th>";}?>
+			<th><?=$config[$serverId]["stats"]["names"][$config[$serverId]["allPlayers"]["defaultStat"]]; ?></th>
+			<th>Value</th>
+		</tr>
+	</tfoot>
+</table>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+	    $('#all-players').dataTable( {
+	    	<?php /* If url rewrites have been disabled */ if ($config[$serverId]["url"]["rewrite"]==false) :?>
+	        "ajax": './ajax/call.php?func=allplayers',
+	        <?php else: ?>
+	        "ajax": '<?=$config[$serverId]["url"]["base"]?>/ajax/?func=allplayers',
+	        <?php endif; ?>
+	        <?php if ($config[$serverId]["server"]["query_enabled"]):?>
+	        "aoColumnDefs": [
+		      { "iDataSort": 3, "aTargets": [ 2 ] },
+		      { "bVisible": false, "aTargets": [ 3 ] }
+		    ]
+		    <?php else: ?>
+		    "aoColumnDefs": [
+		      { "iDataSort": 2, "aTargets": [ 1 ] },
+		      { "bVisible": false, "aTargets": [ 2 ] }
+		    ]
+		    <?php endif; ?>
+	    } );
+	} );	
+</script>
