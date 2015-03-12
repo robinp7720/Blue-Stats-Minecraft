2e6e73015bb3bface6ff3f4f0ab35952a0eddf42
robinp7720 <robin@decker.cx>
2015-03-12 21:20:53 +0000

    Made Query obsolete
    Using only Mc ping now

--- configs/server.php
+++ configs/server.php
@@ -2,6 +2,6 @@
 $config[0]["server"] = array(
 	"ip" => "mysunland.org", /* Server Ip (If hosted on same machine leave as "localhost") */
 	"port" => "25565", /* Server Port (25565 is default) */
-	"query_enabled" => true, /* Weather or not to enable mc query (If enabled players can see who's online) */
+	"query_enabled" => true, /* Weather or not to ping server (If enabled players can see who's online) */
 	"server_name" => "MySunland", /* Not required but shows this in the tab bar */
 );
