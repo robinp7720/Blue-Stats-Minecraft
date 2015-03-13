b35a573ee9865fea1c1978536a23712ab74c727f
robinp7720 <robin@decker.cx>
2015-03-13 18:56:34 +0000

    live tracker
    This actualy tracks more then the live tracker from FluidMCstats!
    Currently it doesnt look to fancy but The basics are there.

---
+++ ajax/dump.php
@@ -0,0 +1,26 @@
+	<?php
+	$columns = array("playtime","wordssaid");
+	$columns = $BlueStats->config["stats"]["id"];
+	$columnsEncoded = "";
+	foreach ($columns as $column){
+		$columnsEncoded .= ", sum(Stats_player.$column) as '$column'";
+	}
+	$query = 
+	"
+Select Stats_players.name $columnsEncoded
+FROM Stats_player 
+INNER JOIN Stats_players 
+WHERE Stats_players.player_id = Stats_player.player_id 
+GROUP by Stats_player.player_id
+	";
+
+	if ($stmt = $mysqli->prepare($query)) {
+		$stmt->execute();
+		$result = $stmt->get_result();
+		while ($row = $result->fetch_assoc()) {
+			$dump[]=$row;
+
+		}
+		$stmt->close();
+	}
+	$output["data"]=$dump;
