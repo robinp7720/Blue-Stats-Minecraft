eba57a2f208b9e08004918bfb40bf0fae5958e8b
robinp7720 <robin@decker.cx>
2015-03-13 16:28:50 +0000

    Fixed error if no one was online

--- include/init_query.php
+++ include/init_query.php
@@ -9,6 +9,8 @@ try
 	$PingInfo = $Ping->Query( );
 	//echo "<pre>".print_r($PingInfo,2)."</pre>";
 	$Online_Players = array();
+	if (!isset($PingInfo["players"]["sample"]))
+		$PingInfo["players"]["sample"]=array();
 	foreach ($PingInfo["players"]["sample"] as $key=>$value){
 		$Online_Players[]=$value["name"];
 	}
