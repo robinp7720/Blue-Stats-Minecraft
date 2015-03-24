0de4ccaac2dcf0cc29074c9db5fb2383e41465d2
robinp7720 <robin@decker.cx>
2015-03-24 19:40:41 +0000

    New Theme

---
+++ themes/custom-green/config.php
@@ -0,0 +1,34 @@
+<?php
+$theme["name"] = "Green";
+$theme["nav"]["enable"]=false;
+$theme["nav"]["color"]=array(
+	"red" => 33,
+	"green" => 150,
+	"blue" => 243
+);
+$theme["nav"]["youtube"]=true;
+$theme["headers"]["color"]=array(
+	"red" => 33,
+	"green" => 150,
+	"blue" => 243
+);
+$theme["pager"]["color"]=array(
+	"red" => 143,
+	"green" => 160,
+	"blue" => 17
+);
+$theme["background"]["use-background-image"]=false;
+$theme["background"]["background-image"]="";
+$theme["background"]["color"]=array(
+	"red" => 153,
+	"green" => 170,
+	"blue" => 17
+);
+
+
+/* True = Full width False = not full width? */
+$theme["container"]["body"]["fluid"]=true;
+$theme["container"]["body"]["container"]=false;
+$theme["container"]["nav"]["fluid"]=true;
+
+$theme["creator"] = "robinp7720";
