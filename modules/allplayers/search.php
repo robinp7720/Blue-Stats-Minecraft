1a1a06182e39aac1755ab9d7ccbb78303c03be87
robinp7720 <robin@decker.cx>
2015-03-27 16:45:34 +0000

    allow selectable stat on all players

---
+++ modules/allplayers/search.php
@@ -0,0 +1,16 @@
+
+<form method="get">
+	<div class="input-group">
+		<select name="stat" class="form-control">
+			<?php
+			foreach($this->config["stats"]["names"] as $statId => $statName){
+				echo '<option value="'.$statId.'">'.$statName.'</option>';
+			}
+			?>
+		</select>
+		<span class="input-group-btn">
+			<button class="form-control" type="submit">Go</button>
+		</span>
+	</div>
+</form>
+<br>
