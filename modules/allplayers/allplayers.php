1443fecb0105f943ad243a1aac4104a9360497ca
robinp7720 <robin@decker.cx>
2015-03-27 16:45:34 +0000

    allow selectable stat on all players

--- modules/allplayers/allplayers.php
+++ modules/allplayers/allplayers.php
@@ -1,10 +1,20 @@
-
+<?php
+if (isset($_GET["stat"])){
+	if (in_array($_GET["stat"],$this->config["stats"]["id"])){
+		$statId = $_GET["stat"];
+	}else{
+		$statId = $this->config["allPlayers"]["defaultStat"];
+	}
+}else{
+	$statId = $this->config["allPlayers"]["defaultStat"];
+}
+?>
 <table class="table table-striped table-bordered" id="all-players">
 	<thead>
 		<tr>
 			<th>Player</th>
 			<?php if($this->config["server"]["query_enabled"]){echo"<th>Status</th>";}?>
-			<th><?=$this->config["stats"]["names"][$this->config["allPlayers"]["defaultStat"]]; ?></th>
+			<th><?=$this->config["stats"]["names"][$statId]; ?></th>
 			<th>Value</th>
 		</tr>
 	</thead>
@@ -12,19 +22,25 @@
 		<tr>
 			<th>Player</th>
 			<?php if($this->config["server"]["query_enabled"]){echo"<th>Status</th>";}?>
-			<th><?=$this->config["stats"]["names"][$this->config["allPlayers"]["defaultStat"]]; ?></th>
+			<th><?=$this->config["stats"]["names"][$statId]; ?></th>
 			<th>Value</th>
 		</tr>
 	</tfoot>
 </table>
-
+<?php
+if (isset($_GET["stat"])){
+	$stat = "&d=".$_GET["stat"];
+}else{
+	$stat = "";
+}
+?>
 <script type="text/javascript">
 	$(document).ready(function() {
 	    $('#all-players').dataTable( {
 	    	<?php /* If url rewrites have been disabled */ if ($this->config["url"]["rewrite"]==false) :?>
-	        "ajax": './ajax/call.php?func=allplayers',
+	        "ajax": './ajax/call.php?func=allplayers<?=$stat?>',
 	        <?php else: ?>
-	        "ajax": '<?=$this->config["url"]["base"]?>/ajax/?func=allplayers',
+	        "ajax": '<?=$this->config["url"]["base"]?>/ajax/?func=allplayers<?=$stat?>',
 	        <?php endif; ?>
 	        <?php if ($this->config["server"]["query_enabled"]):?>
 	        "aoColumnDefs": [
