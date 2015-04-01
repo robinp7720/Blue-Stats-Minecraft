75fc7c06e921915ec0a654ae431c93569f2430ee
robinp7720 <robin@decker.cx>
2015-04-01 11:16:09 +0100

    added comments and removed unused configs

--- configs/general.php
+++ configs/general.php
@@ -1,8 +1,4 @@
 <?php
-
-$config[0]["cache"]["max-age"] = 0;
-$config[0]["cache"]["ajax"]["max-age"] = 0;
-
 /* User player name instead of player id for urls */
 $config[0]["url"]["player"]["useName"]=true;
 $config[0]["site"]["home"]="home";
@@ -11,15 +7,14 @@ $config[0]["site"]["home"]="home";
 $config[0]["url"]["base"] = "http://stats.mysunland.org";
 $config[0]["url"]["rewrite"] = true;
 
-$config[0]["ping"]["cache"]=true;
+$config[0]["ping"]["cache"]=true; /* Weather or not to cache server Ping */
 $config[0]["ping"]["time"] = 10000; /* In Milliseconds */
 
-$config[0]["query"]["cache"]=true;
+$config[0]["query"]["cache"]=true; /* Weather or not to cache server Query */
 $config[0]["query"]["time"] = 100; /* In Milliseconds */
 
 /* Theme settings */
-$config[0]["themes"]["id"]="blue";
-
+$config[0]["themes"]["id"]="bigBlue";
 
 /* Custom navigation bar urls */
 $config[0]["nav"]["tabs"]["custom"] = array(
