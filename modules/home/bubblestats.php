e0caed12d5fc5c43b92bdb2ecd3f745a3cbc4b6e
robinp7720 <robin@decker.cx>
2015-04-24 16:04:26 +0100

    Update bubblestats.php

--- modules/home/bubblestats.php
+++ modules/home/bubblestats.php
@@ -1,4 +1,14 @@
 <?php
+/*
+Html and css for this module was copied from here: http://bootdey.com/snippets/view/Dashboard-user-128 and is released under the MIT licanse
+Copyright (c) 2014 bootdey.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 $mysqli = $this->mysqli->get("BlueStats");
 $stats = array(
 	"playtime" => "clock-o",
