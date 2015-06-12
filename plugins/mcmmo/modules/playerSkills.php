c427f8cb39e589ac4bc96f90796929b50bef4a2b
robinp7720 <robin@decker.cx>
2015-06-12 22:23:19 +0100

    polished up the look

--- plugins/mcmmo/modules/playerSkills.php
+++ plugins/mcmmo/modules/playerSkills.php
@@ -4,29 +4,16 @@ $stats = $plugin->getStat("skills",$_GET["id"]);
 /* Get headers */
 $headers = array();
 unset($stats[0]["user_id"]);
-foreach ($stats[0] as $key => $value){
-	$headers[]=$key;
-}
-
 ?>
 <table class="table">
 	<thead>
-		<tr>
-			<?php foreach ($headers as $key=>$name): ?>
-				<th><?=$name?></th>
-			<?php endforeach; ?>
-		</tr>
 	</thead>
 	<tbody>
-	<?php foreach ($stats as $key=>$playerStats): ?>
+	<?php foreach ($stats[0] as $key=>$val): ?>
 		<tr>
-		<?php
-		unset($playerStats["user_id"]);
-		foreach ($playerStats as $statVal){
-			echo "<td>$statVal</td>";
-		}
-		?>
-	</tr>
+			<th><?=ucfirst($key)?></th>
+			<td><?=$val?></td>
+		</tr>
 	<?php endforeach; ?>
 	</tbody>
 </table>
