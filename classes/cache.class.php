9da5212b822d2c7fc1b9c2ed387beb0dd39de241
robinp7720 <robin@decker.cx>
2015-06-24 16:33:49 +0100

    added add enabled config

--- classes/cache.class.php
+++ classes/cache.class.php
@@ -7,29 +7,36 @@ class cache{
 	{
 		$this->config = new config($mysqli,"cache");
 		$this->config->setDefault("expiry-time", 36000);
+		$this->config->setDefault("enabled", "true");
 		$this->appPath = $appPath;
 	}
 
 	public function cache($content,$name)
-	{
-		$file_name = md5($name).'.html';
-		$time = time();
+	{	
+		if ($this->config->get("enabled")=="true"){
+			$file_name = md5($name).'.html';
+			$time = time();
 
-		$toSave = array("time"=>$time,"content"=>$content);
+			$toSave = array("time"=>$time,"content"=>$content);
 
-		file_put_contents($this->appPath."/cache/$file_name", json_encode($toSave));
+			file_put_contents($this->appPath."/cache/$file_name", json_encode($toSave));
+		}
 	}
 
 	public function reCache($name)
 	{	
-		$file_name = md5($name).'.html';
-		if (file_exists($this->appPath."/cache/$file_name")){
-			$file = file_get_contents($this->appPath."/cache/$file_name");
-			$cache = json_decode($file,true);
-			if ($cache["time"]<time()-$this->config->get("expiry-time")){
-				return true;
+		if ($this->config->get("enabled")=="true"){
+			$file_name = md5($name).'.html';
+			if (file_exists($this->appPath."/cache/$file_name")){
+				$file = file_get_contents($this->appPath."/cache/$file_name");
+				$cache = json_decode($file,true);
+				if ($cache["time"]<time()-$this->config->get("expiry-time")){
+					return true;
+				}else{
+					return false;
+				}
 			}else{
-				return false;
+				return true;
 			}
 		}else{
 			return true;
