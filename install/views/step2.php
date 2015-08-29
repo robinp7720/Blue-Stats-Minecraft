d90b3f6cc16106b6b78bb3cf2f5e0c50faad30cc
robin <robin@decker.cx>
2015-08-30 00:26:40 +0200

    more changes

--- install/views/step2.php
+++ install/views/step2.php
@@ -1,8 +1,12 @@
+<form action="?step=3" method="post" style="overflow: hidden">
 <div class="row">
     <div class="col-md-6">
         <h2>BlueStats Database
             <br><small>(Not lolmewnStats mysql details)</small>
         </h2>
+        <label for="bs-host">Host:</label>
+        <input type="text" class="form-control" id="bs-host" placeholder="Host" name="bs-host" value="">
+
         <label for="bs-username">Username:</label>
         <input type="text" class="form-control" id="bs-username" placeholder="Username" name="bs-username" value="">
 
@@ -13,7 +17,14 @@
         <input type="text" class="form-control" id="bs-db" placeholder="Database name" name="bs-db" value="">
     </div>
     <div class="col-md-6">
-
+        <h2>BlueStats looks <br>
+        <small>Edit/Create themes in /themes/</small>
+        </h2>
+        <label for="theme">Theme: </label>
+        <select id="theme" name="theme" class="form-control">
+            <option value="material">Material</option>
+            <option value="bigBlue">Big Blue</option>
+        </select>
     </div>
 </div>
 
@@ -23,6 +34,9 @@
         <label for="lolstats-enable">Enable: </label>
         <input type="checkbox" id="lolstats-enable" name="lolstats-enable">
         <br>
+        <label for="lolstats-host">Host:</label>
+        <input type="text" class="form-control" id="lolstats-host" placeholder="Host" name="lolstats-host" value="">
+
         <label for="lolstats-username">Username:</label>
         <input type="text" class="form-control" id="lolstats-username" placeholder="Username" name="lolstats-username" value="">
 
@@ -32,7 +46,7 @@
         <label for="lolstats-db">Data Base:</label>
         <input type="text" class="form-control" id="lolstats-db" placeholder="Database name" name="lolstats-db" value="">
 
-        <label for="lolstats-prefix">Data Base:</label>
+        <label for="lolstats-prefix">Table Prefixes:</label>
         <input type="text" class="form-control" id="lolstats-prefix" placeholder="Table prefixes" name="lolstats-prefix" value="">
 
     </div>
@@ -41,6 +55,9 @@
         <label for="mcmmo-enable">Enable: </label>
         <input type="checkbox" id="mcmmo-enable" name="mcmmo-enable">
         <br>
+        <label for="mcmmo-host">Host:</label>
+        <input type="text" class="form-control" id="mcmmo-host" placeholder="Host" name="mcmmo-host" value="">
+
         <label for="mcmmo-username">Username:</label>
         <input type="text" class="form-control" id="mcmmo-username" placeholder="Username" name="mcmmo-username" value="">
 
@@ -50,8 +67,10 @@
         <label for="mcmmo-db">Data Base:</label>
         <input type="text" class="form-control" id="mcmmo-db" placeholder="Database name" name="mcmmo-db" value="">
 
-        <label for="mcmmo-prefix">Data Base:</label>
+        <label for="mcmmo-prefix">Table Prefixes:</label>
         <input type="text" class="form-control" id="mcmmo-prefix" placeholder="Table prefixes" name="mcmmo-prefix" value="">
 
     </div>
 </div>
+    <button type="submit" class="btn btn-success pull-right">Submit</button>
+</form>
--- install/views/step3.php
+++ install/views/step3.php
@@ -1,7 +1,71 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: robin
- * Date: 7/30/2015
- * Time: 10:13 AM
- */
+session_start();
+
+$success = true;
+
+if (isset($_POST["bs-host"]) && isset($_POST["bs-username"]) && isset($_POST["bs-password"]) && isset($_POST["bs-db"])) {
+    /* Connect to MySQL */
+    $mysqli = new mysqli(
+        $_POST["bs-host"],
+        $_POST["bs-username"],
+        $_POST["bs-password"],
+        $_POST["bs-db"]
+    );
+
+    if ($mysqli->connect_error){
+        echo "<b>BlueStats database connetion error:</b> ".$mysqli->connect_error."<br>";
+        $success = false;
+    }else{
+        echo "BlueStats database success!<br>";
+    }
+
+}else{
+    echo "BlueStats database details missing<br>";
+    $success = false;
+}
+if (isset($_POST["lolstats-enable"])&&$_POST["lolstats-enable"]=="on") {
+    if (isset($_POST["lolstats-host"]) && isset($_POST["lolstats-username"]) && isset($_POST["lolstats-password"]) && isset($_POST["lolstats-db"])) {
+        /* Connect to MySQL */
+        $mysqli = new mysqli(
+            $_POST["lolstats-host"],
+            $_POST["lolstats-username"],
+            $_POST["lolstats-password"],
+            $_POST["lolstats-db"]
+        );
+
+        if ($mysqli->connect_error) {
+            echo "<b>Lolmewn Stats database connetion error:</b> " . $mysqli->connect_error . "<br>";
+            $success = false;
+        } else {
+            echo "Lolmewn Stats success!<br>";
+        }
+
+    }else{
+        echo "Lolmewn Stats database details missing<br>";
+        $success = false;
+    }
+}
+
+if (isset($_POST["mcmmo-enable"])&&$_POST["mcmmo-enable"]=="on") {
+    if (isset($_POST["mcmmo-host"]) && isset($_POST["mcmmo-username"]) && isset($_POST["mcmmo-password"]) && isset($_POST["mcmmo-db"])) {
+        /* Connect to MySQL */
+        $mysqli = new mysqli(
+            $_POST["mcmmo-host"],
+            $_POST["mcmmo-username"],
+            $_POST["mcmmo-password"],
+            $_POST["mcmmo-db"]
+        );
+
+        if ($mysqli->connect_error) {
+            echo "<b>Mcmmo database connetion error:</b> " . $mysqli->connect_error . "<br>";
+            $success = false;
+        } else {
+            echo "Mcmmo success!<br>";
+        }
+
+    }else{
+        echo "Mcmmo database details missing<br>";
+        $success = false;
+    }
+}
+$_SESSION = $_POST;
