8fde3fc8bc1f217ac01ea399111b3f41abfc4f84
robin <robin@decker.cx>
2015-09-06 10:33:13 +0200

    Added time ago since change

--- plugins/nameHistory/modules/playernames.php
+++ plugins/nameHistory/modules/playernames.php
@@ -1,8 +1,15 @@
 <table class="table">
 <?php
 foreach ($plugin->getNames($player->uuid) as $name){
-  echo "<tr>";
+    echo "<tr>";
     echo "<td>{$name["name"]}</td>";
+    if (isset($name["changedToAt"])){
+        $timeago = (time()*1000) - $name["changedToAt"];
+        $timeago = secondsToTime($timeago/1000);
+        echo "<td>{$timeago} ago</td>";
+    }else{
+        echo "<td>Original</td>";
+    }
     echo "</tr>";
 }
 ?>
