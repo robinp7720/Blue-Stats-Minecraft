1a199faf8bc34b61146d8bf9d66a83d766c7c0b6
robinp7720 <robin@decker.cx>
2015-10-20 17:39:04 +0200

    Layout for new admin panel

--- admin/pages/index.php
+++ admin/pages/index.php
@@ -1,5 +1,10 @@
 <div class="row">
-
-    <canvas id="TrafficChart" class="col-md-6"></canvas>
-
+    <div class="col-md-6 chartContainer">
+        <h2>Traffic</h2>
+        <div id="TrafficChart"></div>
+    </div>
+    <div class="col-md-6 chartContainer">
+        <h2>Load</h2>
+        <div id="LoadChart"></div>
+    </div>
 </div>
