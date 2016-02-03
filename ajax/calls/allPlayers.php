9eaf78d866b756931db2287e0612fe5149099531
robinp7720 <robin@decker.cx>
2016-02-03 21:06:31 +0000

    Updated yeti theme and made player names links

--- ajax/calls/allPlayers.php
+++ ajax/calls/allPlayers.php
@@ -1,4 +1,11 @@
 <?php
 $base_plugin = $config->get("base_plugin");
 $plugin = $plugins[$base_plugin];
+
+$url = str_replace('{page}','player', $config->get("player-url", 'BlueStats-Url'));
+$url = str_replace('{player}', $config->get("useUUID", 'BlueStats-Url')=="true"? '{UUID}' : '{NAME}', $url);
+
+$output['info'] = [
+    'url' => $url
+];
 $output['data'] = $plugin->getUsers(0, 10000, MYSQLI_NUM);
