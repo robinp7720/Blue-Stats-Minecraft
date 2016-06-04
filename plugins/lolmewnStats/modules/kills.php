e6d806f3b1c5edef5c697778e94621eef3ae8f12
Robin <robin@decker.cx>
2016-06-04 12:31:19 +0200

    Add player kills module closes #42

---
+++ plugins/lolmewnStats/modules/kills.php
@@ -0,0 +1,19 @@
+<table class="table table-sorted" id="pvp-stats">
+    <thead>
+    <th>Weapon</th>
+    <th>World</th>
+    <th>Amount</th>
+    <th>Entity Type</th>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($plugin->getStat('kill', $player->uuid, False) as $stat):?>
+        <tr>
+            <td><?=$stat['weapon']?></td>
+            <td><?=$stat['world']?></td>
+            <td><?=$stat['value']?></td>
+            <td><?=$stat['entityType']?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
