fc065e720ba56fe1fa2224d1ef8c9841decb58b4
robin <robin@decker.cx>
2017-01-08 14:04:56 +0100

    Detect if system is posix compliant

--- admin/pages/security.php
+++ admin/pages/security.php
@@ -1,30 +1,38 @@
-<?php
-$processUser = posix_getpwuid(posix_geteuid());
-$secure = true;
-?>
-<h2>Security</h2>
+<?php if (function_exists('posix_getpwuid')): ?>
+    <?php
+    $processUser = posix_getpwuid(posix_geteuid());
+    $secure = true;
+    ?>
+    <h2>Security</h2>
 
-<?php if ($processUser['name'] == "root"): ?>
-    <div class="alert alert-danger" role="alert">
-        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-        <span class="sr-only">Danger:</span>
-        Webserver is running as root. This is a major security risk
-    </div>
-    <?php $secure = false; ?>
-<?php endif; ?>
+    <?php if ($processUser['name'] == "root"): ?>
+        <div class="alert alert-danger" role="alert">
+            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            <span class="sr-only">Danger:</span>
+            Webserver is running as root. This is a major security risk
+        </div>
+        <?php $secure = false; ?>
+    <?php endif; ?>
 
-<?php if ($processUser['gecos'] == "root"): ?>
-    <div class="alert alert-danger" role="alert">
-        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-        <span class="sr-only">Danger:</span>
-        Webserver is user is part of superuser group. This is a major security risk
-    </div>
-    <?php $secure = false; ?>
-<?php endif; ?>
+    <?php if ($processUser['gecos'] == "root"): ?>
+        <div class="alert alert-danger" role="alert">
+            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            <span class="sr-only">Danger:</span>
+            Webserver is user is part of superuser group. This is a major security risk
+        </div>
+        <?php $secure = false; ?>
+    <?php endif; ?>
 
-<?php if ($secure): ?>
-    <div class="alert alert-success" role="alert">
-        <span class="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span>
-        No security threats detected
+    <?php if ($secure): ?>
+        <div class="alert alert-success" role="alert">
+            <span class="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span>
+            No security threats detected
+        </div>
+    <?php endif; ?>
+
+<?php else: ?>
+    <div class="alert alert-wanring" role="alert">
+        <span class="glyphicon glyphicon glyphicon-sign" aria-hidden="true"></span>
+        Windows is not officialy supported
     </div>
 <?php endif; ?>
