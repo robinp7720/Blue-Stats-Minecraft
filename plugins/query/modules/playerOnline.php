37cf515b72bbe74579041bb9c3a2ee324723d218
Zeyphros <robin@decker.cx>
2017-03-03 16:46:03 +0100

    Removed unused config setting

--- plugins/query/modules/playerOnline.php
+++ plugins/query/modules/playerOnline.php
@@ -1,8 +1,4 @@
 <?php
-$config->setDefault("image-src", "http://cravatar.eu/avatar/{NAME}/64");
-
-$imageSrc = $config->get("image-src");
-
 if (in_array($player->name, $plugin->onlinePlayers())) {
     echo '<span class="label label-success">Online</span>';
 } else {
