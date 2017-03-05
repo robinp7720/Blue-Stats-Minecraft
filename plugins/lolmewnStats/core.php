5fd951c51435c3033798914faf6deb0a63baaa9d
Zeyphros <robin@decker.cx>
2017-03-05 12:14:56 +0100

    Fix when ONLY_FULL_GROUP_BY is enabled. Fixes #76

--- plugins/lolmewnStats/core.php
+++ plugins/lolmewnStats/core.php
@@ -76,7 +76,7 @@ class lolmewnStats extends MySQLplugin
 SELECT
   {$this->prefix}{$this->plugin['indexTable']}.name as name,
   {$this->prefix}{$stat}.{$this->plugin['idColumn']},
-  sum({$this->prefix}{$stat}.value) as `value` 
+  max({$this->prefix}{$stat}.value) as `value` 
 FROM {$this->prefix}{$stat}
 INNER JOIN `{$this->prefix}{$this->plugin['indexTable']}` on {$this->prefix}{$stat}.{$this->plugin['idColumn']} = {$this->prefix}{$this->plugin['indexTable']}.{$this->plugin['idColumn']}
 GROUP BY {$this->prefix}{$this->plugin['indexTable']}.name, {$this->prefix}{$stat}.{$this->plugin['idColumn']}
