2f121d98eae0a7e78970466b9302d17f59f1a541
Robin <robin@decker.cx>
2017-05-08 18:06:32 +0200

    Player identification functions added

--- classes/plugin/pluginPlayer.php
+++ classes/plugin/pluginPlayer.php
@@ -9,47 +9,77 @@
 class pluginPlayer
 {
 
-    public $database = [
-        "identifier" => "id", // Can be id, uuid or name. Used to get stats based on id. name or uuid
-        "index" => [ // Define the table which contains used data
-            "table" => "players",
-            "columns" => [
-                "uuid" => "uuid",
-                "name" => "name",
-                "id" => "id",
-            ]
-        ]
-    ];
+    public $database; // Loaded from parent plugin class
+    private $mysql;   // Loaded from parent plugin class
 
     /**
      * pluginPlayer constructor.
      * @param $database array Database layout to identify players in the database
+     * @param $mysql mysqli Connection to mysql server for plugin
      */
-    public function __construct($database)
+    public function __construct($database, $mysql)
     {
         $this->database = $database;
+        $this->mysql = $mysql;
     }
 
     /**
      * This function uses the user id to get the username
      * @param int $id ID of user as defined by plugin
+     * @return string Username of player
      */
     public function getName($id)
     {
+        $mysqli = $this->mysql;
+        $stmt = $mysqli->stmt_init();
 
+        // Select the name from the player identification table using the id column for identification
+        $query = "SELECT {$this->database["index"]["columns"]["name"]} FROM {$this->database["prefix"]}{$this->database["index"]["table"]} WHERE {$this->database["index"]["columns"]["id"]} = ?";
+        if ($stmt->prepare($query)) {
+            $stmt->bind_param("s", $id);
+            $stmt->execute();
+            $stmt->bind_result($output);
+            $stmt->fetch();
+
+            // If there is an error log it
+            if ($stmt->error && DEBUG)
+                print($stmt->error);
+
+            $stmt->close();
+            return $output;
+        }
+        return false;
     }
 
     /**
      * This function uses the user id to get the uuid of a user
      * @param int $id ID of user as defined by plugin
+     * @return string UUID of player
      */
     public function getUUID($id)
     {
+        $mysqli = $this->mysql;
+        $stmt = $mysqli->stmt_init();
 
+        // Select the uuid from the player identification table using the id column for identification
+        $query = "SELECT {$this->database["index"]["columns"]["uuid"]} FROM {$this->database["prefix"]}{$this->database["index"]["table"]} WHERE {$this->database["index"]["columns"]["id"]} = ?";
+        if ($stmt->prepare($query)) {
+            $stmt->bind_param("s", $id);
+            $stmt->execute();
+            $stmt->bind_result($output);
+            $stmt->fetch();
+
+            // If there is an error log it
+            if ($stmt->error && DEBUG)
+                print($stmt->error);
+
+            $stmt->close();
+            return $output;
+        }
+        return false;
     }
 
-    /**
-     *
+    /*
      * @param String $user UUID or username of player
      */
 
@@ -60,19 +90,57 @@ class pluginPlayer
 
     /**
      * @param String $uuid UUID of player
+     * @return int User id
      */
 
     public function getIDfromUUID($uuid)
     {
+        $mysqli = $this->mysql;
+        $stmt = $mysqli->stmt_init();
 
+        // Select the id from the player identification table using the uuid column for identification
+        $query = "SELECT {$this->database["index"]["columns"]["id"]} FROM {$this->database["prefix"]}{$this->database["index"]["table"]} WHERE {$this->database["index"]["columns"]["uuid"]} = ?";
+        if ($stmt->prepare($query)) {
+            $stmt->bind_param("s", $uuid);
+            $stmt->execute();
+            $stmt->bind_result($output);
+            $stmt->fetch();
+
+            // If there is an error log it
+            if ($stmt->error && DEBUG)
+                print($stmt->error);
+
+            $stmt->close();
+            return $output;
+        }
+        return false;
     }
 
     /**
      * @param String $name Username of player
+     * @return int User ID
      */
 
     public function getIDfromName($name)
     {
+        $mysqli = $this->mysql;
+        $stmt = $mysqli->stmt_init();
 
+        // Select the id from the player identification table using the name column for identification
+        $query = "SELECT {$this->database["index"]["columns"]["id"]} FROM {$this->database["prefix"]}{$this->database["index"]["table"]} WHERE {$this->database["index"]["columns"]["name"]} = ?";
+        if ($stmt->prepare($query)) {
+            $stmt->bind_param("s", $name);
+            $stmt->execute();
+            $stmt->bind_result($output);
+            $stmt->fetch();
+
+            // If there is an error log it
+            if ($stmt->error && DEBUG)
+                print($stmt->error);
+
+            $stmt->close();
+            return $output;
+        }
+        return false;
     }
 }
