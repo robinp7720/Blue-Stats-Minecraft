aa9319b970661d50a8961989e3cdc2366d68f902
Robin <robin@decker.cx>
2017-06-06 18:19:18 +0200

    link player pages and add player heads

--- modules/highscores.php
+++ modules/highscores.php
@@ -5,17 +5,25 @@ $this->loadPlugin("Statz");
 $plugin = $this->plugins['Statz'];
 
 foreach ($plugin->database['stats'] as $stat => $info) {
-    echo "<h2>$info[name]</h2>";
+    echo "<div class='col-md-6'><h2>$info[name]</h2>";
 
     $table = new Table;
 
     foreach ($plugin->stats->statList($stat, 10) as $row) {
+        $username = $plugin->player->getName($plugin->player->getID($row['uuid']));
+        if ($this->bluestats->url->useUUID) {
+            $name = "<a href=\"" . $this->bluestats->url->player($row['uuid']) . "\"><img src=\"https://minotar.net/helm/$username/32.png\" alt=\"\"> {$username}</a>";
+        } else {
+            $name = "<a href=\"" . $this->bluestats->url->player($username) . "\"><img src=\"https://minotar.net/helm/$username/32.png\" alt=\"\"> {$username}</a>";
+        }
         $table->addRecord(
-            $plugin->player->getName($plugin->player->getID($row['uuid'])),
+            $name,
             $row['aggregate']
         );
     }
     $table->makeHeader("Player", $info['name']);
 
     echo $table->tableToHTML();
+
+    echo "</div>";
 }
