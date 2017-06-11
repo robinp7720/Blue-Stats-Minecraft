e5b1d504a64b95ed2d3acde6f0d76018bffe5764
Robin <robin@decker.cx>
2017-06-11 13:44:46 +0200

    Added group display to highscores

--- modules/highscores.php
+++ modules/highscores.php
@@ -1,44 +1,72 @@
 <?php
+
+$render = function($module, $plugin, $stat) {
+    $info = $plugin->database['stats'][$stat];
+
+    $output = "<div class='col-md-6'><h4>$info[name]</h4>";
+
+    $table = new Table();
+
+    $options = [
+        "selectMethod" => isset($info["summary"]) ? $info["summary"] : "sum",
+    ];
+
+    foreach ($plugin->stats->statList($stat, 10, $options) as $row) {
+        if ($plugin->database['identifier'] == "id") {
+            $username = $plugin->player->getNamefromID($row['id']);
+            $uuid     = $plugin->player->getUUIDfromID($row['id']);
+        }
+        else {
+            $username = $plugin->player->getNamefromID($plugin->player->getID($row['id']));
+            $uuid     = $row['id'];
+        }
+        if ($this->bluestats->url->useUUID) {
+            $name = "<a href=\"" . $module->bluestats->url->player($uuid) . "\"><img src=\"https://minotar.net/helm/$username/32.png\" alt=\"\"> {$username}</a>";
+        }
+        else {
+            $name = "<a href=\"" . $module->bluestats->url->player($username) . "\"><img src=\"https://minotar.net/helm/$username/32.png\" alt=\"\"> {$username}</a>";
+        }
+        $table->addRecord(
+            $name,
+            $row['aggregate']
+        );
+    }
+    $table->makeHeader("Player", $info['name']);
+
+    $output .= $table->tableToHTML(FALSE);
+    $output .= "</div>";
+
+    return $output;
+
+};
+
 /** @var module $this */
 foreach ($this->bluestats->plugins as $plugin) {
     /** @var \BlueStats\API\plugin $plugin */
     if (!$plugin::$isMySQLplugin)
         break;
-    echo "<h2>{$plugin->name}</h2><div class='row'>";
-    foreach ($plugin->database['stats'] as $stat => $info) {
-        echo "<div class='col-md-6'><h3>$info[name]</h3>";
 
-        $table = new Table();
+    echo "<h2>{$plugin->name}</h2>";
 
-        $options = [
-            "selectMethod" => isset($info["summary"]) ? $info["summary"] : "sum",
-        ];
+    if (!isset($plugin->database['groups'])) $plugin->database['groups'] = [];
 
-        foreach ($plugin->stats->statList($stat, 10, $options) as $row) {
-            if ($plugin->database['identifier'] == "id") {
-                $username = $plugin->player->getNamefromID($row['id']);
-                $uuid     = $plugin->player->getUUIDfromID($row['id']);
-            }
-            else {
-                $username = $plugin->player->getNamefromID($plugin->player->getID($row['id']));
-                $uuid     = $row['id'];
-            }
-            if ($this->bluestats->url->useUUID) {
-                $name = "<a href=\"" . $this->bluestats->url->player($uuid) . "\"><img src=\"https://minotar.net/helm/$username/32.png\" alt=\"\"> {$username}</a>";
-            }
-            else {
-                $name = "<a href=\"" . $this->bluestats->url->player($username) . "\"><img src=\"https://minotar.net/helm/$username/32.png\" alt=\"\"> {$username}</a>";
-            }
-            $table->addRecord(
-                $name,
-                $row['aggregate']
-            );
+    foreach ($plugin->database['groups'] as $groupId => $group) {
+        echo "<h3>{$group['name']}</h3>";
+        echo "<div class='row'>";
+        foreach ($group['stats'] as $stat) {
+            echo $render($this, $plugin, $stat);
         }
-        $table->makeHeader("Player", $info['name']);
-
-        echo $table->tableToHTML(FALSE);
-
         echo "</div>";
+    }
+
+    echo "<div class='row'>";
+
+    foreach ($plugin->database['stats'] as $stat => $info) {
+        // Set default stat options
+        if (!isset($info['display'])) $info['display'] = TRUE;
+        if (!$info['display']) break;
+
+        echo $render($this, $plugin, $stat);
     }
     echo "</div>";
 }
