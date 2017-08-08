5c01495ff0d912686849d0eada333ea3ab991028
Robin <robin@decker.cx>
2017-08-08 22:11:53 +0200

    Added function to get total sum of stat of all players

--- classes/plugin/pluginStats.php
+++ classes/plugin/pluginStats.php
@@ -133,6 +133,48 @@ class pluginStats {
         return FALSE;
     }
 
+    public function sum($stat) {
+        $mysqli = $this->mysql;
+        $stmt   = $mysqli->stmt_init();
+
+        $aggregateColumn = "";
+        $aggregateID = "";
+
+        foreach ($this->database["stats"][$stat]["values"] as $id => $info) {
+            if ($info['aggregate']) {
+                $aggregateColumn = $info['column'];
+                $aggregateID = $id;
+                break;
+            }
+        }
+
+        // Get aggregate type
+        $aggregateType = "sum";
+        if (isset($this->database["stats"][$stat]["values"][$aggregateID]["aggregate_type"]))
+            $aggregateType = $this->database["stats"][$stat]["values"][$aggregateID]["aggregate_type"];
+
+        $query = "SELECT {$aggregateType}($aggregateColumn) as total FROM {$this->database["prefix"]}{$this->database["stats"][$stat]["database"]}";
+
+        if ($stmt->prepare($query)) {
+            $stmt->execute();
+
+            $stmt->bind_result($count);
+            $stmt->fetch();
+
+            // If there is an error log it
+            if ($stmt->error && DEBUG)
+                print($stmt->error);
+
+            $stmt->close();
+
+            return $count;
+        }
+        if ($stmt->error && DEBUG)
+            print($stmt->error);
+
+        return FALSE;
+    }
+
     /**
      * @param pluginPlayer $pluginPlayer
      */
