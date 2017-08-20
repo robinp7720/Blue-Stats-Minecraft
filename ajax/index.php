a28da1db61d127c165ef5829dee35fbae6a7cd96
Robin <robin@decker.cx>
2017-08-21 01:33:33 +0200

    Don't output errors

--- ajax/index.php
+++ ajax/index.php
@@ -1,6 +1,10 @@
 <?php
 define('DEBUG', FALSE);
 
+// Hide errors. If errors get outputted the json will not be correctly formatted
+error_reporting(0);
+ini_set("display_errors", 0);
+
 $appPath = __DIR__ . "/..";
 
 /* Classes */
